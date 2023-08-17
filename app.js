e66639f3081915b6229772fa3abc72f26a55a317
marcus.bishop.1995@live.co.uk <root@DESKTOP-VMTTBRK>
2023-08-17 14:04:07 +0100

    add missing brackets and catch next to first error handler

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.patch("/api/articles/:article_id", patchArticles);
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
