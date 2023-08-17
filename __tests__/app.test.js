3d8a059c1f72859f11f1487adc676ad0a77b33e0
marcus.bishop.1995@live.co.uk <root@DESKTOP-VMTTBRK>
2023-08-17 15:18:58 +0100

    add condense granular tests to use matchObject matcher

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,8 @@ const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const endpointsJSON = require("../endpoints.json");
-const endpointsJS = JSON.parse(JSON.stringify(endpointsJSON));
+const { expect } = require("@jest/globals");
+
 
 
 //PRE AND POST TEST FUNCTIONS
@@ -29,7 +30,6 @@ describe("Northcoders News API ", () => {
     test("Should return an array", () => {
       return request(app)
         .get("/api/topics")
-        .expect(200)
         .then(({ body }) => {
           expect(Array.isArray(body)).toBe(true);
         });
@@ -37,7 +37,6 @@ describe("Northcoders News API ", () => {
     test("Should return an array with length equal to total number of topics in database", () => {
       return request(app)
         .get("/api/topics")
-        .expect(200)
         .then(({ body }) => {
           expect(body.length).toBe(3);
         });
@@ -45,7 +44,6 @@ describe("Northcoders News API ", () => {
     test(`Each object in the  returned array should have keys of 'slug' and 'description'`, () => {
       return request(app)
         .get("/api/topics")
-        .expect(200)
         .then(({ body }) => {
           body.forEach((topic) => {
             expect(topic).toHaveProperty("slug");
@@ -56,7 +54,6 @@ describe("Northcoders News API ", () => {
     test("Each object in the returned array should only have 2 keys", () => {
       return request(app)
         .get("/api/topics")
-        .expect(200)
         .then(({ body }) => {
           body.forEach((topic) => {
             expect(Object.keys(topic).length).toBe(2);
@@ -66,65 +63,23 @@ describe("Northcoders News API ", () => {
   });
   describe("GET /api", () => {
     test("Should return status 200 when accessed", () => {
-      return request(app).get("/api").expect(200);
+      return request(app)
+        .get("/api")
+        .expect(200);
     });
     test("Should return an object", () => {
       return request(app)
         .get("/api")
-        .expect(200)
         .then((endpoints) => {
           expect(typeof endpoints).toBe("object");
         });
     });
-    test("Returned object should have keys of known endpoints", () => {
+    test("Should return an object with all the data from the endpoints.json", () => {
       return request(app)
         .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-          expect(Object.keys(body)).toEqual(Object.keys(endpointsJS));
-        });
-    });
-    test("Returned object should not have extra undesired keys", () => {
-      return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-          expect(Object.keys(body).length).toBe(
-            Object.keys(endpointsJS).length
-          );
-        });
-    });
-    test("Each nested object should have the correct keys", () => {
-      return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-          const endpoints = Object.keys(body);
-          endpoints.forEach((endpoint) => {
-            expect(Object.keys(body[endpoint])).toEqual(
-              Object.keys(endpointsJS[endpoint]),
-              expect(Object.keys(body[endpoint])).toEqual([
-                "description",
-                "queries",
-                "requestBodyFormat",
-                "exampleResponse",
-              ])
-            );
-          });
-        });
-    });
-    test("Each nested object should not have extra undesired keys", () => {
-      return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-          const endpoints = Object.keys(body);
-          endpoints.forEach((endpoint) => {
-            expect(Object.keys(body[endpoint]).length).toBe(
-              Object.keys(endpointsJS[endpoint]).length
-            );
-          });
-        });
-    });
+        .then(({ body }) => { 
+          expect(body).toMatchObject(endpointsJSON)
+        })
+    })
   });
 });
