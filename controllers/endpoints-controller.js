5a47b1cd7445d1f8e0a8aef2da9f82ecd9a4001d
marcus.bishop.1995@live.co.uk <root@DESKTOP-VMTTBRK>
2023-08-17 17:09:05 +0100

    remove deleteComments files, accidently brought them to main

--- controllers/endpoints-controller.js
+++ controllers/endpoints-controller.js
@@ -4,10 +4,8 @@ const jsonEndpoints = require("../endpoints.json");
 //FUNCTION
 function getEndpoints(req, res, next) {
   return res.status(200).send(jsonEndpoints)
-    .catch((err) => {
-      next(err);
-    });
 }
+
 
 //EXPORTS
 module.exports = {getEndpoints}
---
+++ models/delete-comment-model.js
@@ -0,0 +1,30 @@
+// //REQUIRE
+// const db = require("../db/connection.js");
+
+// //FUNCTION
+// function removeComment(comment_id) {
+//   const onlyDigitsRegex = /(^[0-9]+$)/;
+//   if (!onlyDigitsRegex.test(comment_id)) {
+//     return Promise.reject({
+//       status: 400,
+//       msg: `${comment_id} is not a valid comment_id`,
+//     });
+//   }
+//     return db.query
+//         (`DELETE FROM comments
+//           WHERE comments.comment_id = $1
+//           RETURNING *;`, [comment_id]
+//     ).then(({rows}) => {
+//         if (rows.length === 1) {
+//             Promise.resolve()
+//         } else {
+//             return Promise.reject({
+//                 status: 400,
+//                 msg: `No comment deleted`
+//             })
+//         }
+//     })
+// }
+
+// //EXPORTS
+// module.exports = {removeComment}
